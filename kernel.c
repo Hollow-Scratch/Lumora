ba5c17c5e756acae2990e601351b961dd2ea4726
Hollow-Scratch <258008941+Hollow-Scratch@users.noreply.github.com>
2026-07-28 18:56:49 +0900

    Add initial kernel boot support

---
+++ kernel.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+static uint16_t* const VGA = (uint16_t*)0xB8000;
+
+void kernel_main(void)
+{
+    const char* msg = "Welcome to Lumora OS!";
+    uint8_t color = 0x0F;
+
+    for (int i = 0; msg[i] != '\0'; i++)
+    {
+        VGA[i] = ((uint16_t)color << 8) | msg[i];
+    }
+
+    while (1)
+    {
+        __asm__ volatile ("hlt");
+    }
+}
