06a4cec1c9b1db5a0a54511df5899b393b153980
hollow <258008941+Hollow-Scratch@users.noreply.github.com>
2026-07-28 19:00:49 +0900

    Add initial kernel boot support (#1)

---
+++ boot.asm
@@ -0,0 +1,25 @@
+; Multiboot header
+section .multiboot
+align 4
+dd 0x1BADB002
+dd 0x0
+dd -(0x1BADB002 + 0x0)
+
+section .text
+global start
+extern kernel_main
+
+start:
+    cli
+    mov esp, stack_top
+    call kernel_main
+
+.hang:
+    hlt
+    jmp .hang
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
